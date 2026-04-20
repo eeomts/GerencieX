40910156c5f91d9106f7151240a5cb541eac9aad
eeomts <mateolimoreira7@gmail.com>
2026-04-19 22:06:45 -0300

    refatora script de migração e adiciona suporte a subcomandos

--- storage/database/table/properties.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE properties (
-    id        INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    block     VARCHAR(10)  NOT NULL,
-    number    VARCHAR(10)  NOT NULL,
-    floor     TINYINT UNSIGNED NOT NULL DEFAULT 0,
-    created_at DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at datetime,
-    deleted tinyint not null default 0
-);
