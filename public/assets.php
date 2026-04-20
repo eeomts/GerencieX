4f35fefeea51d3b9b4d566531f53147bdb0d2f9a
eeomts <mateolimoreira7@gmail.com>
2026-04-19 22:12:08 -0300

    refatora a função renderAssets para usar caminhos locais e remove a verificação de existência

--- public/assets.php
+++ public/assets.php
@@ -2,17 +2,13 @@
 
 declare(strict_types=1);
 
-if (!function_exists('renderAssets')) {
-    function renderAssets(string $basePath = ''): void
-    {
-        $basePath = rtrim($basePath, '/');
-        $appJsPath = ($basePath !== '' ? $basePath : '') . '/assets/js/app.js';
 
-        echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/jquery-steps@1.1.0/build/jquery.steps.css">' . PHP_EOL;
-        echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">' . PHP_EOL;
-        echo '<script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>' . PHP_EOL;
-        echo '<script src="https://cdn.jsdelivr.net/npm/jquery-steps@1.1.0/build/jquery.steps.min.js"></script>' . PHP_EOL;
-        echo '<script src="https://cdn.jsdelivr.net/npm/toastify-js"></script>' . PHP_EOL;
-        echo '<script src="' . htmlspecialchars($appJsPath, ENT_QUOTES, 'UTF-8') . '"></script>' . PHP_EOL;
-    }
+function renderAssets(): void
+{
+    echo '<link rel="stylesheet" href="/assets/vendor/jquery-steps/jquery.steps.css">' . PHP_EOL;
+    echo '<link rel="stylesheet" href="/assets/vendor/toastify/toastify.css">' . PHP_EOL;
+    echo '<script src="/assets/vendor/jquery/jquery.min.js"></script>' . PHP_EOL;
+    echo '<script src="/assets/vendor/jquery-steps/jquery.steps.min.js"></script>' . PHP_EOL;
+    echo '<script src="/assets/vendor/toastify/toastify.js"></script>' . PHP_EOL;
+    echo '<script src="/assets/js/app.js"></script>' . PHP_EOL;
 }
