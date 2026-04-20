7e58b5ad931946641c16b8c305e9670058499e66
eeomts <mateolimoreira7@gmail.com>
2026-04-19 22:12:53 -0300

    remove arquivos de configuração de exemplo e adiciona novo arquivo credentials.example

---
+++ config/credentials.example.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+define('DB_DRIVER', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_PORT', 3306);
+define('DB_DATABASE', 'your_database');
+define('DB_USER', 'your_user');
+define('DB_PASSWORD', 'your_password');
+define('DB_CHARSET', 'utf8mb4');
