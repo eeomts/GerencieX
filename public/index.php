b4b0e668a3608c0ac6692b5a1f3f6ef5e58e8d5a
eeomts <mateolimoreira7@gmail.com>
2026-04-19 22:14:11 -0300

    adiciona estrutura básica do projeto com controladores, modelos e rotas

--- public/index.php
+++ public/index.php
@@ -1,4 +1,20 @@
-<?php 
-require __DIR__ . '/vendor/autoload.php';
+<?php
 
-?>
+declare(strict_types=1);
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+session_start();
+
+require_once __DIR__ . '/assets.php';
+
+require __DIR__ . '/../config/config.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+use Src\Core\Router;
+
+$router = new Router();
+
+require __DIR__ . '/../config/routes.php';
+
+$router->dispatch();
