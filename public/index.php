a00690c57937b412a7665781fb4c6b676138990b
eeomts <mateolimoreira7@gmail.com>
2026-04-21 23:51:45 -0300

    requires basicos

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@ ini_set('display_errors', '1');
 
 session_start();
 
+require_once __DIR__ . '/helpers.php';
 require_once __DIR__ . '/assets.php';
 
 require __DIR__ . '/../config/config.php';
@@ -14,6 +15,7 @@ require __DIR__ . '/../vendor/autoload.php';
 use Src\Core\Router;
 
 $router = new Router();
+$router->setMiddlewareAliases(require __DIR__ . '/../config/middleware.php');
 
 require __DIR__ . '/../config/routes.php';
 
