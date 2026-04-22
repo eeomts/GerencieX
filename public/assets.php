c545f44480dbeb972abeedda649ce85dd366a8d1
eeomts <mateolimoreira7@gmail.com>
2026-04-21 23:50:26 -0300

    assets do projeto

--- public/assets.php
+++ public/assets.php
@@ -2,13 +2,13 @@
 
 declare(strict_types=1);
 
-
 function renderAssets(): void
 {
-    echo '<link rel="stylesheet" href="/assets/vendor/jquery-steps/jquery.steps.css">' . PHP_EOL;
-    echo '<link rel="stylesheet" href="/assets/vendor/toastify/toastify.css">' . PHP_EOL;
-    echo '<script src="/assets/vendor/jquery/jquery.min.js"></script>' . PHP_EOL;
-    echo '<script src="/assets/vendor/jquery-steps/jquery.steps.min.js"></script>' . PHP_EOL;
-    echo '<script src="/assets/vendor/toastify/toastify.js"></script>' . PHP_EOL;
-    echo '<script src="/assets/js/app.js"></script>' . PHP_EOL;
+        echo '<link rel="stylesheet" href="' . asset('css/style.css') . '">' . PHP_EOL;
+        echo '<link rel="stylesheet" href="' . asset('vendor/jquery-steps/jquery.steps.css') . '">' . PHP_EOL;
+        echo '<link rel="stylesheet" href="' . asset('vendor/toastify/toastify.css') . '">' . PHP_EOL;
+        echo '<script src="' . asset('vendor/jquery/jquery.min.js') . '"></script>' . PHP_EOL;
+        echo '<script src="' . asset('vendor/jquery-steps/jquery.steps.min.js') . '"></script>' . PHP_EOL;
+        echo '<script src="' . asset('vendor/toastify/toastify.js') . '"></script>' . PHP_EOL;
+        echo '<script src="' . asset('js/app.js') . '"></script>' . PHP_EOL;
 }
